e1fe9d47e3074c5697b791f7d301b0e11ceacac2
Anh Dang <anhcdang@gmail.com>
2021-07-12 13:51:34 -0700

    commit tree 016c19a99f6d62a5f05b537666f1e401ba603fa3

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,8 @@
 ActiveRecord::Schema.define(version: 20210708181115) do
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "full_name"
+    t.string "username"
     t.string "password_digest"
   end
 
